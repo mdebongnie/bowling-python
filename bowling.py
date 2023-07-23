3776b1b87dde738114bb7335c5957eddafbc9d1c
mdebongnie <debongnie.mathieu@gmail.com>
2023-07-23 15:54:27 +0200

    implemented a full solution and added a few tests

--- bowling.py
+++ bowling.py
@@ -1,9 +1,30 @@
 class BowlingGame():
     def __init__(self):
+
+        self.rolls = []
         pass
     
     def roll(self, pins_down: int) -> None:
+
+        self.rolls.append(pins_down)
         pass
 
     def score(self) -> int:
-        return 0
+        
+        points = []
+        for i in range(10):
+
+            points.append(self.rolls.pop(0))
+            
+            if points[-1] == 10:
+                # Strike
+                points[-1] += self.rolls[0] + self.rolls[1]
+                continue
+
+            points[-1] += self.rolls.pop(0)
+
+            if points[-1] == 10:
+                # Spare
+                points[-1] += self.rolls[0]
+                      
+        return sum(points)
--- bowling.test.py
+++ bowling.test.py
@@ -23,5 +23,68 @@ class TestStringMethods(unittest.TestCase):
 
         self.assertEqual(score, 90)
 
+    def test_should_be_able_to_score_a_game_with_no_strikes_and_a_spare(self):
+        rolls = [3, 6, 3, 6, 3, 6, 3, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 94)
+
+    def test_should_be_able_to_score_a_game_with_no_strikes_and_two_spares(self):
+        rolls = [3, 6, 3, 6, 3, 6, 3, 6, 4, 6, 4, 6, 3, 6, 3, 6, 3, 6, 3, 6]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 99)
+
+    def test_should_be_able_to_score_a_game_with_a_strike_and_no_spare(self):
+        rolls = [3, 6, 3, 6, 3, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 100)
+    
+    def test_should_be_able_to_score_a_game_with_a_strike_and_no_spare(self):
+        rolls = [3, 6, 10, 3, 6, 3, 6, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 110)
+
+    def test_should_be_able_to_score_a_game_with_two_strikes_and_a_spare(self):
+        rolls = [3, 6, 10, 3, 6, 4, 6, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 121)
+
+    def test_should_be_able_to_score_a_game_with_two_consecutive_strikes_and_a_spare(self):
+        rolls = [3, 6, 10, 10, 4, 6, 10, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 137)
+
+    def test_should_be_able_to_score_a_game_with_spare_at_end(self):
+        rolls = [3, 6, 10, 10, 4, 6, 10, 3, 6, 3, 6, 3, 6, 3, 6, 4, 6, 3]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 141) 
+
+    def test_should_be_able_to_score_a_game_with_strike_at_end(self):
+        rolls = [3, 6, 10, 10, 4, 6, 10, 3, 6, 3, 6, 3, 6, 3, 6, 10, 3, 6]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 147) 
+    
+    def test_should_be_able_to_score_a_perfect_game(self):
+        rolls = [10 for i in range(12)]
+
+        score = self._roll_a_game_and_score(rolls)
+
+        self.assertEqual(score, 300) 
+
 if __name__ == '__main__':
     unittest.main()
