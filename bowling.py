ec8feafadc953b5a43cb7470123c64bafb650fa6
mdebongnie <debongnie.mathieu@gmail.com>
2023-07-23 16:01:17 +0200

    implemented a full solution with least lines possible and added a few test

--- bowling.py
+++ bowling.py
@@ -1,9 +1,14 @@
 class BowlingGame():
     def __init__(self):
+
+        self.rolls = []
         pass
     
     def roll(self, pins_down: int) -> None:
+
+        self.rolls.append(pins_down)
         pass
 
     def score(self) -> int:
-        return 0
+        
+        return sum([self.rolls.pop(0) + self.rolls[0] + self.rolls[1] if self.rolls[0] == 10 else self.rolls.pop(0) + self.rolls.pop(0) + self.rolls[0] if self.rolls[0] + self.rolls[1] == 10 else self.rolls.pop(0) + self.rolls.pop(0) for i in range(10)])
